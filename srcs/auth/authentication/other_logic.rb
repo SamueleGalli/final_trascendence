ba8ad1732dd089a5c1c5fd320d3ecbc5cf63a180
SamueleGalli <samuele.aggeggio@gmail.com>
2025-01-03 09:56:24 +0100

    the right one this time

---
+++ srcs/auth/authentication/other_logic.rb
@@ -0,0 +1,48 @@
+require 'json'
+require 'pg'
+require 'colorize'
+load ((File.file? '/var/www/common/BetterPG.rb') ? '/var/www/common/BetterPG.rb' : '../../common_tools/tools/BetterPG.rb')
+
+LOGIN = BetterPG::SimplePG.new "users", ["login_name TEXT", "name TEXT", "email TEXT", "image TEXT", "token TEXT"]
+
+module Other_logic
+  def not_found(response)
+    response.status = 404
+    response.content_type = 'application/json'
+    response.write({ success: false, error: "Not Found" }.to_json)
+  end
+
+  def get_user_data_from_oauth_provider(token)
+    uri = URI("https://api.intra.42.fr/v2/me")
+    request = Net::HTTP::Get.new(uri)
+    request["Authorization"] = "Bearer #{token}"
+
+    response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) do |http|
+      http.request(request)
+    end
+  
+    if response.code.to_i == 200
+      user_data = JSON.parse(response.body)
+    else
+      puts "Errore API 42: #{response.code}"
+      return nil
+    end
+
+        name = user_data['usual_full_name']
+        email = user_data['email']
+        image = user_data['image']
+        login_name = user_data['login']
+        puts "\nDati ricevuti dall'API di 42:".green
+        puts "       name: #{name}".green
+        puts "       email: #{email}".green
+        puts "       image: #{image}".green
+        puts "  login_name: #{login_name}".green
+    if image.nil? || image.empty?
+      image = 'nulla'
+    end
+    STDERR.puts "adding to database: " + [login_name, name, email, image, token].to_s
+    # LOGIN.addValues([login_name, name, email, image, token], ["login_name", "name", "email", "image", "token"])
+    LOGIN.addValues ["'"+login_name.to_s+"'", "'"+name.to_s+"'", "'"+email.to_s+"'", "'"+token.to_s+"'"], ["login_name", "name", "email", "token"]
+    return { 'login_name' => login_name, 'name' => name, 'email' => email, 'avatar_url' => image }
+  end
+end
